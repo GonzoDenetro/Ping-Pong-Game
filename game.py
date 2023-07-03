a53c5405c347dd91421f9b6c3d7a402b493ebad9
Gonzo Denetro <gonzi.ceron.denetro@gmailcom>
2023-07-02 22:59:38 -0600

    Class for our paddle created

--- game.py
+++ game.py
@@ -1,6 +1,15 @@
 import pygame
 pygame.init()
 
+
+class Paddle:
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+
 def run():
     
     #SCREEN SETUP
