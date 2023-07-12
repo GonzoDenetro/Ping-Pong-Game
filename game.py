38db1b7c1d830db8d51eba551670ed7ba322cef8
Gonzo Denetro <gonzi.ceron.denetro@gmailcom>
2023-07-12 15:35:44 -0600

    Collision en x axis

--- game.py
+++ game.py
@@ -42,12 +42,18 @@ class Ball:
     
     def draw(self, screen):
         pygame.draw.circle(screen, [255, 255, 255], [self.x, self.y], self.radius)
+    
+    def movement(self):
+        #Para mover la pelota va a ser en base en que parte de la raqueta haya tocado la pelota
+        self.x += self.x_velocity
 
 
-def draw_game(screen, player1, player2, width, height):
+def draw_game(screen, player1, player2, ball, width, height):
     screen.fill([0, 0, 0])
     player1.draw(screen)
     player2.draw(screen)
+    
+    ball.draw(screen)
     
     #DRAWING DASH LINE
     for i in range(10, height, height//20):
@@ -56,6 +62,29 @@ def draw_game(screen, player1, player2, width, height):
     
     pygame.display.update()
 
+
+def handle_collision(ball, left_paddle, right_paddle, height):
+    #Cuando toquemos la parte arriba o abajo cambiamos de dirección
+    if ball.y + ball.radius >= height:
+        ball.y_velocity *= -1
+    elif ball.y - ball.radius <= 0:
+        ball.y_velocity *= -1
+    
+    #LEFT SIDE
+    if ball.x_velocity < 0:
+        #Vamos a checar que las coordenadas de la pelota sean iguales a las de la raqueta
+        if ball.y >= left_paddle.y and ball.y <= left_paddle.y + left_paddle.height: #Checamos colisione en "y"
+            if ball.x - ball.radius <= left_paddle.x + left_paddle.width:
+                ball.x_velocity *= -1
+    
+    #RIGHT SIDE
+    else:
+        if ball.y >= right_paddle.y and ball.y <= right_paddle.y + right_paddle.height:
+            if ball.x + ball.radius >= right_paddle.x:
+                ball.x_velocity *= -1
+                print("plank right")
+    
+    print(f'x velocity: {ball.x_velocity}, x: {ball.x}')
 
 
 def run():
@@ -73,6 +102,8 @@ def run():
     player1 = Paddle(10, screen_height//2 - 100//2, 20, 100)
     player2 = Paddle(screen_width - 30, screen_height//2 - 100//2, 20, 100)
     
+    ball = Ball(screen_width//2, screen_height//2, 7)
+    
     running = True
     while running:
         
@@ -84,8 +115,11 @@ def run():
         
         player1.movement(key, -1)
         player2.movement(key, 1)    
+        ball.movement()
         
-        draw_game(screen, player1, player2, screen_width, screen_height)
+        handle_collision(ball, player1, player2, screen_height)
+        
+        draw_game(screen, player1, player2, ball, screen_width, screen_height)
         
         for event in pygame.event.get():
             if event.type ==  pygame.QUIT:
